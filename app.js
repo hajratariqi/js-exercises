8e58a52ebef1bffded15d9d03130d9a5426f95f3
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-06-09 21:02:47 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+// var firstNumber = +prompt("Enter a first integer");
+// var secondNumber = +prompt("Enter a integer");
+//  if (firstNumber > secondNumber){
+//     console.log(firstNumber + " is large interger")
+// } else if (firstNumber < secondNumber){
+//     console.log(secondNumber + " is large interger")
+// }else if (firstNumber == secondNumber){
+//     console.log(firstNumber + " and " + secondNumber + ": both are equal")
+// }
+
+
+
+
+
+
+
+
+
+// input a number from user & state whether the number is positive, negative or zero.
+
+// var num = +prompt("enter an number");
+
+// if (num == 0){
+//     console.log("number is zero" + num)
+// } else if (num > 0){
+//         console.log("number is positive " + num)
+// }else if(num < 0){
+//     console.log("number is negative " + num)
+// }
+
+
+
+// Check vowel or not 
+
+
+var vowel = prompt("Enter a character");
+if(vowel.length <= 1 && vowel == "a" || vowel == "e" || vowel == "i" || vowel == "o" || vowel == "u"){
+    console.log(vowel + " is vowel")
+}
+else{
+    console.log(vowel + " is not vowel")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
